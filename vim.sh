4e82ae732e63e9f33f703a8143d888658839691d
LiuYang <18738460107@163.com>
2021-01-27 20:23:53 +0800

    create autoable scirpt

---
+++ vim.sh
@@ -0,0 +1,7 @@
+cp .vimrc ~/
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+if [[ $? -ne 0 ]];then
+  cp -r .vim ~/
+fi
+
+vim -c "PlugInstall | p | p"
