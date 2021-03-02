4129c2b93d0805e566fc52f25b5fbec236b1c291
LiuYang <18738460107@163.com>
2021-03-02 21:51:01 +0800

    Update vim.sh
    create ~/.vim/autoload

--- vim.sh
+++ vim.sh
@@ -1,4 +1,5 @@
 cp .vimrc ~/
+mkdir -p ~/.vim/autoload
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 if [[ $? -ne 0 ]];then
   cp -r .vim ~/
